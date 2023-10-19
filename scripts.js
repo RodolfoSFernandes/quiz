986d7dad5ef24e296cfb1355515766c962ddb975
Rodolfo Fernandes <rodolfo.fernandes@navarraaluminio.com>
2023-10-19 14:14:49 +0100

    Atualização final

--- scripts.js
+++ scripts.js
@@ -41,4 +41,92 @@ const questions = [
     },
 ];
 
+const questionElement = document.querySelector("#question");
+const choiceElements = document.querySelectorAll(".choice");
+const nextButton = document.querySelector("#next");
+const scoreElement = document.querySelector("#score");
+const wrongElement = document.querySelector("#wrong");
 
+let currentQuestion = 0;
+let score = 0;
+let wrong = 0;
+let answerChosen = false;
+
+function loadQuestion() {
+  const currentQuestionData = questions[currentQuestion];
+  questionElement.innerText = currentQuestionData.question;
+
+  const choices = shuffleArray(currentQuestionData.choices);
+
+  for (let i = 0; i < choiceElements.length; i++) {
+    choiceElements[i].innerText = choices[i];
+  }
+
+  answerChosen = false;
+}
+
+function shuffleArray(array) {
+  let currentIndex = array.length;
+  let temporaryValue;
+  let randomIndex;
+
+  while(0 !== currentIndex) {
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex -= 1;
+
+    temporaryValue = array[currentIndex]
+    array[currentIndex] = array[randomIndex]
+    array[randomIndex] = temporaryValue
+  }
+
+  return array;
+}
+
+function checkAnswer(e) {
+
+  if(answerChosen) return;
+
+  answerChosen = true;
+
+  if(e.target.innerText === questions[currentQuestion].answer) {
+    score++;
+    scoreElement.innerText = `Pontuação: ${score}`;
+    alert("Correto!");
+  } else {
+    wrong++;
+    wrongElement.innerText = `Erros: ${wrong}`;
+    alert(`Errado! A resposta correta é: ${questions[currentQuestion].answer}`);
+  }
+}
+
+choiceElements.forEach((btn) => {
+  btn.addEventListener("click", checkAnswer);
+});
+
+nextButton.addEventListener("click", () => {
+  if(!answerChosen) {
+    alert("Por favor, responda à questão!");
+    return;
+  }
+
+  currentQuestion++;
+
+  if (currentQuestion < questions.length) {
+    loadQuestion();
+  } else {
+    alert(`Fim do jogo! Acertou ${score} de ${questions.length} perguntas.`);
+    
+    restartQuiz();
+  }
+});
+
+function restartQuiz() {
+  currentQuestion = 0;
+  score = 0;
+  wrong = 0;
+  scoreElement.innerText = `Pontuação: 0`;
+  wrongElement.innerText = `Erros: 0`;
+  loadQuestion();
+}
+
+loadQuestion();
